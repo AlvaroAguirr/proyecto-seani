2e203f8e0c4aca8f6e0b47a3df98d66a321b5109
alvaro aguirre <alguirre2@gmail.com>
2024-02-16 15:46:55 -0600

    02 App Career - Career Model

---
+++ siani/career/admin.py
@@ -0,0 +1,12 @@
+#aqui se configura el panel de administrador de base de datos
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import Career
+
+class CareerAdmin(admin.ModelAdmin):
+    list_display = ['short_name', 'level']
+    list_filter = ['level']
+admin.site.register(Career,CareerAdmin)
+
